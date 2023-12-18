19fd37b252bf8f3e4dc47b0b7747d73cd638e8bd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-18 09:47:57 +0200

    get all and single users

--- src/server.js
+++ src/server.js
@@ -1,12 +1,47 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 const PORT = 3000;
 
+// DATA
+const users = [
+  { id: 1, name: 'Serbentautas', town: 'Vilnius', isDeleted: false },
+  { id: 2, name: 'Lenteja', town: 'Kaunas', isDeleted: false },
+  { id: 3, name: 'James', town: 'London', isDeleted: false },
+];
+
+// Middleware
+app.use(morgan('dev'));
+
+// routes
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+// GET /api/users - grazina visus vartotojus
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+// GET /api/users/4 - gets user
+app.get('/api/users/:userId', (req, res) => {
+  const userId = +req.params.userId;
+  // surasti obj su id === userId
+  const found = users.find((uObj) => uObj.id === userId);
+  // jei neradom
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `user not found with id ${userId}`,
+    });
+    return;
+  }
+  res.json(found);
+});
+// DELETE /api/users/1 - deletes user
+
+// Run the server
 app.listen(PORT, () =>
   console.log(`Server runing on http://localhost:${PORT}`)
 );
